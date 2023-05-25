952f7b3e4fa19522b03c055a6b4d1349db7980bb
Murod Erkinov <murodjonerkinov2005@gmail.com>
2023-05-26 00:41:12 +0500

    added io_service

---
+++ lib/services/io_service.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+IOService io = IOService();
+
+class IOService {
+  void _console(String text) {
+    stdout.write(text);
+  }
+
+  String inputText(String text) {
+    _console(text);
+    return stdin.readLineSync() ?? "";
+  }
+
+  int inputNumber(String text) {
+    _console(text);
+    return int.tryParse(stdin.readLineSync() ?? "0") ?? 0;
+  }
+
+  String inputTextImportant(String text) {
+    String result = "";
+    while (result.isEmpty) {
+      result = inputText(text);
+    }
+    return result;
+  }
+
+  String setPassword(String text) {
+    String result = "";
+    bool test;
+    do {
+      test = false;
+      result = inputText(text);
+      if (result.length < 8) {
+        print("Parol uzunligi 8 ga teng yoki undan uzun bo'lishi kerak...");
+        test = true;
+      }
+      if (!result.contains(RegExp(r"[A-Z]"))) {
+        print("Parolda kamida 1 ta katta harf bo'lishi kerak!");
+        test = true;
+      }
+      if (!result.contains(RegExp(r"[a-z]"))) {
+        print("Parolda kamida 1 ta kichik harf bo'lishi kerak!");
+        test = true;
+      }
+      if (!result.contains(RegExp(r"[\d]"))) {
+        print("Parolda kamida 1 ta raqam bo'lishi kerak!");
+        test = true;
+      }
+      if (!result.contains(RegExp(r"[^A-z\d]"))) {
+        print("Parolda kamida 1 ta maxsus belgi bo'lishi kerak!");
+        test = true;
+      }
+    } while (test);
+    return result;
+  }
+}
+
+Taymer ty = Taymer();
+
+class Taymer {
+  void taymer(String text, int seconds) {
+    for (int i = seconds; i > 0; i--) {
+      print("$text. $i...");
+      sleep(Duration(seconds: 1));
+    }
+  }
+}
