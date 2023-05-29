27ce8128aff6371e683812036153a32f83de95ab
erkinoff <murodjonerkinov2005@gmail.com>
2023-05-29 16:04:53 +0500

    .

--- bin/payme.dart
+++ bin/payme.dart
@@ -1,5 +1,8 @@
 import 'package:payme/pages/intro_page.dart' show IntroPage;
 
+///Uchbu dastur havaskor dasturchi tomonidan yozildi
+///Xato va kamchiliklari bo'lishi mumkin!!!
+
 void main(List<String> arguments) {
   //Dasturni boshlash
   IntroPage();
